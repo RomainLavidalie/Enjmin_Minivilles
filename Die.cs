943a317dddc3cdb8e3dd6eb3dd6a2641cfa63eec
Yaribot <47753213+Yaribot@users.noreply.github.com>
2021-10-28 13:24:24 +0200

    Die Class

---
+++ Die.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Enjmin_Minivilles_Console
+{
+    class Die
+    {
+        private Random random;
+        private int face;
+
+        public Die()
+        {           
+            face = 7;
+            random = new Random();
+        }
+
+        public int Tossing()
+        {
+            int diceValue;
+            
+            diceValue = random.Next(0, face);
+
+            return diceValue;
+        }
+
+        public override string ToString()
+        {
+            string toString = String.Format("+---+\n");
+            toString += String.Format("| {0} |\n", Tossing());
+            toString += String.Format("+---+\n");
+
+            return toString;
+        }
+
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,10 +1,21 @@
-﻿namespace Enjmin_Minivilles_Console
+﻿using System;
+using System.Collections.Generic;
+
+namespace Enjmin_Minivilles_Console
 {
     internal class Program
     {
         public static void Main(string[] args)
         {
+            //Die die = new Die();
 
+            //for(int i = 0; i < 2; i++)
+            //{
+            //    Console.WriteLine(die);
+            //}
+
+            //Console.WriteLine(die);
+            Console.ReadLine();
         }
     }
 }
