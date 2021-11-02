6e8cbeeaa69c02485ea6711bd65d7ec6ce27c08a
coco3167 <corent1.creully@sfr.fr>
2021-11-02 11:05:51 +0100

    Amélioration affichage des dés

--- Die.cs
+++ Die.cs
@@ -10,13 +10,28 @@ namespace Enjmin_Minivilles_Console
     {
         private Random random;
         private int face;
+        private List<List<int>> tableauDe = new List<List<int>>();
 
         public Die()
         {           
             face = 7;
             random = new Random();
+            TabInit();
         }
 
+        private void TabInit()
+        {
+            tableauDe.Add(new List<int>{2,3,4,5,6});
+            tableauDe.Add(new List<int>{});
+            tableauDe.Add(new List<int>{4,5,6});
+            tableauDe.Add(new List<int>{6});
+            tableauDe.Add(new List<int>{1,3,5});
+            tableauDe.Add(new List<int>{6});
+            tableauDe.Add(new List<int>{4,5,6});
+            tableauDe.Add(new List<int>{});
+            tableauDe.Add(new List<int>{2,3,4,5,6});
+        }
+        
         public int Tossing()
         {
             int diceValue;
@@ -26,13 +41,38 @@ namespace Enjmin_Minivilles_Console
             return diceValue;
         }
 
-        public override string ToString()
+        public string[] ToString()
         {
-            string toString = String.Format("+---+\n");
-            toString += String.Format("| {0} |\n", Tossing());
-            toString += String.Format("+---+\n");
+            int Face = Tossing();
+            string[] lignes = new string[5];
+            if (Face > 6)
+            {
+                return (new string[]{Face.ToString()});
+            }
+            else
+            {
+                lignes[0] = " _________ ";
+                for (int loop = 0; loop < 3; loop++)
+                {
+                    lignes[loop+1] += "|";
+                    for (int loop1 = 0; loop1 < 3; loop1++)
+                    {
+                        if (tableauDe[loop*3 + loop1].Contains(Face))
+                        {
+                            lignes[loop+1] += " * ";
+                        }
+                        else
+                        {
+                            lignes[loop+1] += "   ";
+                        }
+                    }
 
-            return toString;
+                    lignes[loop+1] += "|";
+                }
+
+                lignes[4] += " --------- ";
+                return lignes;
+            }
         }
 
 
