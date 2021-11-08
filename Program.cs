d76fe5604bfbf74bc07eb9d0de1a2f7f61860ea1
coco3167 <corent1.creully@sfr.fr>
2021-11-08 14:29:37 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 
 namespace Enjmin_Minivilles_Console
@@ -11,6 +11,7 @@ namespace Enjmin_Minivilles_Console
             int nbPlayer;
             int nbDice;
             int nbBot;
+            int choixFin;
             
             Console.WriteLine("MINIVILLE");
             Console.WriteLine("Nombre de joueurs ?");
@@ -30,8 +31,14 @@ namespace Enjmin_Minivilles_Console
             {
                 Console.Write("Mauvais format de réponse veuillez recommencer : ");
             }
+            
+            Console.WriteLine("Choisissez votre nombre de pièces pour la victoire : 10 (1), 20 (2), 30 (3).");
+            while (!Int32.TryParse(Console.ReadLine(), out choixFin) && choixFin > 0 && choixFin < 4)
+            {
+                Console.Write("Mauvais format de réponse veuillez recommencer : ");
+            }
 
-            Game game = new Game(nbPlayer, nbDice, nbBot);
+            Game game = new Game(nbPlayer, nbDice, nbBot, choixFin);
 
             Console.WriteLine(game);
         }
